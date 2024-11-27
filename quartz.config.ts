84edfbe901357e86ead234b4964442edbae2d872
1ncarnati0n <pattern.langue@gmail.com>
2024-11-27 20:47:30 +0900

    Update all

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "1ncarnati0n.pages.dev",
+    baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: true,
